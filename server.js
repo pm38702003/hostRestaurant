43e1db600d74ed47a1e46fb310f4307b1d25d253
Ian Walston <ikwalston@comcast.net>
2019-01-09 10:56:56 -0500

    added express server.js and instructions for project

---
+++ server.js
@@ -0,0 +1,35 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//get request for home page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//get request to reservations
+app.get("/reservations", function(req, res) {
+  res.json();
+});
+
+//post request to reservations
+app.post('/reservations', (req, res) => {
+  // const name = req.body.name
+})
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
